a536ab31d5ed7ae44ff1d87c398b5720c0a4d2c6
Ravi Souza <perfectbase@users.noreply.github.com>
2023-01-14 12:15:22 +0900

    cdk init

---
+++ cdk-base-template/test/cdk-base-template.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkBaseTemplate from '../lib/cdk-base-template-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-base-template-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkBaseTemplate.CdkBaseTemplateStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
